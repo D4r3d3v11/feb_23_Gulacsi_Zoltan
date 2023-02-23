f201dbc421c80be80f5da0d9a3b68b40891adeb0
D4r3d3v11 <gulacsizoltan14@gmail.com>
2023-02-23 18:05:21 +0100

    2.feladat

--- Feb_23_ora.java
+++ Feb_23_ora.java
@@ -1,16 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.feb_23_ora;
 
-/**
- *
- * @author Zolka
- */
 public class Feb_23_ora {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public static void main(String[] args) {
+    int osszeg = elso10szamOszege();
+    String kimenet = "Az első 10 szám összege:" + osszeg + "\n";
+    kiir(kimenet);
+    int szam1 = 7, szam2 = 4;
+    kiir("%d + %d = %d".formatted(szam1,szam2, a+b));
 }
+
+    private static void kiir(String szoveg) {
+        System.out.print(szoveg);
+    }
+
+    private static int elso10szamOszege() {
+        int osszeg = 0;
+        for ( int i= 0; i<10; i++){
+            osszeg +=1;
+        }
+        return osszeg;
+    }       
+    
+    private static int osszead(int a, int b){
+        return a+b;
+    }
+    
