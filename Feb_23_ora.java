fb22fb00becaa0b58ad0a2334e4b384cd439f39b
D4r3d3v11 <gulacsizoltan14@gmail.com>
2023-02-23 17:42:01 +0100

    ora feladat

---
+++ Feb_23_ora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.feb_23_ora;
+
+/**
+ *
+ * @author Zolka
+ */
+public class Feb_23_ora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
